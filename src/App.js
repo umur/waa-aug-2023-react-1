9078e07aa6b7a187d1ed25b6381a97a95525fbc9
abraham <abraham.mathewos.meja@gmail.com>
2023-08-10 10:57:26 -0500

    Add page navigation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import './App.css';
 import Category from "./Pages/Category";
 import User from "./Pages/User";
+import {Link, Route, Routes} from "react-router-dom";
+import UnknownPage from "./UnknownPage";
 
 function App() {
-    const categoryData = {
-        id: 1,
-        name: 'Electronics',
-        products: [
-            {
-                id: 1,
-                name: 'Smartphone',
-                price: 499.99,
-                rating: 4.5,
-                category: {
-                    id: 1,
-                    name: 'Electronics',
-                },
-                reviews: [
-                    { id: 1, comment: 'Great phone!' },
-                    { id: 2, comment: 'Battery life could be better.' },
-                ],
-            },
-        ],
-    };
   return (
     <div className="App">
-        {/*<User/>*/}
-        <Category/>
+        <h1>Simple CRUD app</h1>
+        <ul>
+            <li><Link to="/user">Users</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/error">Show 404 page</Link></li>
+        </ul>
+
+        <Routes>
+            <Route path="/user" element={<User/>}/>
+            <Route path="/category" element={<Category/>}/>
+            <Route path="*" element={<UnknownPage/>}/>
+        </Routes>
     </div>
   );
 }
